a738993195504e2a7038eb6bd936b4bb848d4501
mohasin3639 <mohasinsayyad3639@gmail.com>
2022-12-22 19:25:02 +0530

    ADDED: Program to check monthly wage of employee till a given condition is reached

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -4,30 +4,33 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("welcome to employee wage computation program");
         int empHrs;
-        int dailyWage;
-        int monthlyWage = 0;
         int empWagePerHrs = 20;
         int workingDaysPerMonth = 20;
-        for(int day = 0; day < workingDaysPerMonth; day++) {
-            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-            switch (empCheck) {
-                case 1:
-                    System.out.println("Employee is Present");
-                     empHrs= 8;
-                    break;
-                case 2:
-                    System.out.println("Employee is Part-Time Present");
-                    empHrs = 4;
-                    break;
-                default:
-                    System.out.println("Employee is Absent");
-                    empHrs= 0;
+        int maximumHrsInMonth = 100;
+        int totalEmployeeHrs = 0;
+        int totalWorkingDays = 0;
+        while (totalEmployeeHrs <= maximumHrsInMonth &&
+                totalWorkingDays < workingDaysPerMonth) {
+            totalWorkingDays++; {
+                int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+                switch (empCheck) {
+                    case 1:
+                        System.out.println("Employee is Present");
+                        empHrs = 8;
+                        break;
+                    case 2:
+                        System.out.println("Employee is Part-Time Present");
+                        empHrs = 4;
+                        break;
+                    default:
+                        System.out.println("Employee is Absent");
+                        empHrs = 0;
+                }
+                totalEmployeeHrs = empHrs + totalEmployeeHrs;
+                System.out.println("Day:" + totalWorkingDays + " EmpHour: " + empHrs);
             }
-            dailyWage = empHrs * empWagePerHrs;
-            monthlyWage = dailyWage + monthlyWage;
-            System.out.println("Daily wage of employee is "+ dailyWage);
-        }
-        System.out.println("-------------------");
-        System.out.println("Monthly employee wage is "+ monthlyWage);
+            int totalEmployeeWage = totalEmployeeHrs * empWagePerHrs;
+            System.out.println("Total Employee Wage " + totalEmployeeWage);
         }
     }
+}
