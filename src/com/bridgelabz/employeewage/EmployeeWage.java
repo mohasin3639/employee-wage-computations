7b8b5655a60721f356c7133a4c4890a2e97b27dc
mohasin3639 <mohasinsayyad3639@gmail.com>
2022-12-22 16:28:44 +0530

    ADDED: program to check employee daily wage

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -4,11 +4,18 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("welcome to employee wage computation program");
         int IS_FULL_TIME = 1;
+        int empHrs = 8;
+        int dailyWage = 0;
+        int empWagePerHrs = 20;
         double empCheck = Math.floor(Math.random() * 10) % 2;
 
-        if (empCheck == IS_FULL_TIME)
+        if (empCheck == IS_FULL_TIME) {
             System.out.println("Employee is Present");
-        else
+            dailyWage = empHrs * empWagePerHrs;
+        } else {
             System.out.println("Employee is Absent");
+
+        }
+        System.out.println("the daily wage of employee is "+dailyWage);
     }
 }
