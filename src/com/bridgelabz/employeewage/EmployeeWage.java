d2cdc1b192d4b66ebb10a8fb6b769b6043f86fcb
mohasin3639 <mohasinsayyad3639@gmail.com>
2022-12-22 18:18:32 +0530

    ADDED: Program to check part-time employee daily wage

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -4,18 +4,24 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("welcome to employee wage computation program");
         int IS_FULL_TIME = 1;
-        int empHrs = 8;
-        int dailyWage = 0;
+        int IS_PART_TIME = 2;
+        int empHrsFullTime = 8;
+        int empHrsPartTime = 4;
+        int dailyWage ;
         int empWagePerHrs = 20;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
+        double empCheck = Math.floor(Math.random() * 10) % 3;
 
         if (empCheck == IS_FULL_TIME) {
             System.out.println("Employee is Present");
-            dailyWage = empHrs * empWagePerHrs;
-        } else {
-            System.out.println("Employee is Absent");
-
+            dailyWage = empHrsFullTime * empWagePerHrs;
+            System.out.println("the daily wage of employee is " + dailyWage);
         }
-        System.out.println("the daily wage of employee is "+dailyWage);
+        else if (empCheck == IS_PART_TIME){
+                System.out.println("Employee is Part-Time Present");
+                dailyWage = empHrsPartTime * empWagePerHrs;
+                System.out.println("the daily wage of employee is "+dailyWage);
+            } else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
