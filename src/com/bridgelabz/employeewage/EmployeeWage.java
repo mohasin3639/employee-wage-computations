302011afeb191dfd0cba4ea6b6fcd8982a042566
mohasin3639 <mohasinsayyad3639@gmail.com>
2022-12-22 18:31:33 +0530

    ADDED: Program to check daily wage of employee using switch case

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -3,25 +3,25 @@ package com.bridgelabz.employeewage;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("welcome to employee wage computation program");
-        int IS_FULL_TIME = 1;
-        int IS_PART_TIME = 2;
         int empHrsFullTime = 8;
         int empHrsPartTime = 4;
         int dailyWage ;
         int empWagePerHrs = 20;
-        double empCheck = Math.floor(Math.random() * 10) % 3;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
-        if (empCheck == IS_FULL_TIME) {
+        switch (empCheck) {
+            case 1:
             System.out.println("Employee is Present");
             dailyWage = empHrsFullTime * empWagePerHrs;
             System.out.println("the daily wage of employee is " + dailyWage);
-        }
-        else if (empCheck == IS_PART_TIME){
+            break;
+            case 2:
                 System.out.println("Employee is Part-Time Present");
                 dailyWage = empHrsPartTime * empWagePerHrs;
-                System.out.println("the daily wage of employee is "+dailyWage);
-            } else {
-            System.out.println("Employee is Absent");
+                System.out.println("the daily wage of employee is " + dailyWage);
+             break;
+            default:
+                System.out.println("Employee is Absent");
+            }
         }
     }
-}
